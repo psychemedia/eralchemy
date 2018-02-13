835b08ec14d919427e8b7419217307355947a2f0
Tony Hirst <tony.hirst@gmail.com>
2018-02-13 20:17:49 +0000

    splines=ortho setting randomly redirects edges

--- eralchemy/cst.py
+++ eralchemy/cst.py
@@ -2,6 +2,8 @@
 
 """
 All the constants used in the module.
+
+ splines=ortho in graph[] causes random edge direction?
 """
 TABLE = '"{}" [label=<<FONT FACE="Helvetica"><TABLE BORDER="0" CELLBORDER="1"' \
         ' CELLPADDING="4" CELLSPACING="0">{}{}</TABLE></FONT>>];'
@@ -11,7 +13,7 @@ FONT_TAGS = '<FONT>{}</FONT>'
 # Used for each row in the table.
 ROW_TAGS = '<TR><TD{}>{}</TD></TR>'
 GRAPH_BEGINNING = (' digraph {\n'
-                   '    graph [rankdir=LR, splines=ortho];\n'
+                   '    graph [rankdir=LR];\n'
                    '    node [label=\"\\N\",\n'
                    '        shape=plain\n'
                    '    ];\n'
